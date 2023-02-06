be5c819b753bf79251ead21cfcc8b9955b1286ed
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-02-06 17:29:55 +0500

    commit

--- resources/views/themes/default/index.blade.php
+++ resources/views/themes/default/index.blade.php
@@ -631,7 +631,7 @@
                          data-ts-map-leaflet-attribution="&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://cartodb.com/attributions'>CartoDB</a>"
                          data-ts-map-zoom-position="bottomright"
                          data-ts-map-scroll-wheel="1"
-                         data-ts-map-zoom="13"
+                         data-ts-map-zoom="10"
                          data-ts-map-center-latitude="41.1145"
                          data-ts-map-center-longitude="71.4468"
                          data-ts-locale="uz-UZ"
